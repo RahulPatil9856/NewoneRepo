9b8a03733290cf252d290168f452d7910b3427fd
Rahul <95970103+RahulPatil9856@users.noreply.github.com>
2022-08-26 16:09:17 +0530

    Update LoginTest.java
    You should do these changes

--- src/Test/LoginTest.java
+++ src/Test/LoginTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
-
+// Rahul Ramkrushna Patil
 public static void main(String arg[])
 {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Desktop\\SeleniumTesting\\chromedriver_win32 (3)chromedriver.exe");
