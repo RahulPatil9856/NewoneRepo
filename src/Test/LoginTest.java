823e5343321cc882f6c40fea55e96f4ec1489ab2
RahulPatil9856 <rahulpatil9856@gmail.com>
2022-08-26 12:30:02 +0530

    First commit

---
+++ src/Test/LoginTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+public static void main(String arg[])
+{
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Desktop\\SeleniumTesting\\chromedriver_win32 (3)chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com");
+	}
+}
